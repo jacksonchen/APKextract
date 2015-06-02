0eb491e04268b29cac8d57ccc6cd1dc860883a7c
Jackson Chen <tiger2chen@gmail.com>
2015-06-02 17:10:28 -0600

    Finishes extraction script

--- extract.py
+++ extract.py
@@ -1,19 +1,52 @@
 import sys
 import os
 from subprocess import call
+
 appsrepo = sys.argv[2]
-targetDir = sys.argv[3]
+unpacked = sys.argv[3]
+manifest = sys.argv[4]
+code = sys.argv[5]
+uixml = sys.argv[6]
+appsrepoPath = os.path.abspath(appsrepo)
+unpackedPath = os.path.abspath(unpacked)
+manifestPath = os.path.abspath(manifest)
+codePath = os.path.abspath(code)
+uixmlPath = os.path.abspath(uixml)
 
 for root, dirs, files in os.walk(sys.argv[1]):
   if not dirs:
+    print "=====No subdirectories====="
     dirPath = os.path.abspath(root)
-    appsrepoPath = os.path.abspath(appsrepo)
-    targetDirPath = os.path.abspath(targetDir)
-    UnpackCOMMAND = "python " + appsrepoPath + "/tools/apktool_executor.py " + dirPath + " " + targetDirPath
-    print UnpackCOMMAND
+
+    UnpackCOMMAND = "python " + appsrepoPath + "/tools/apktool_executor.py " + dirPath + " " + unpackedPath
     call(UnpackCOMMAND, shell=True)
+
+    ManifestCOMMAND = "python " + appsrepoPath + "/tools/copy_manifest.py " + unpackedPath + " " + manifestPath
+    call(ManifestCOMMAND, shell=True)
+
+    CodeCOMMAND = "python " + appsrepoPath + "/smali-methods-finder/smali_invoked_methods.py " + unpackedPath + "/ " + codePath + "/"
+    call(CodeCOMMAND, shell=True)
+
+    uixmlCOMMAND = "python " + appsrepoPath + "/ui-xml/ui_xml.py " + unpackedPath + " -o " + uixmlPath
+    call(uixmlCOMMAND, shell=True)
+    break
+
   else:
+    print "=====Multiple subdirectories====="
     for dir in dirs:
-      dirPath = os.path.join(root, dir)
-      UnpackCOMMAND = "python " + appsrepo + "/tools/apktool_executor.py " + dirPath + " " + targetDir
+      dirPath = os.path.abspath(os.path.join(root, dir))
+
+      UnpackCOMMAND = "python " + appsrepoPath + "/tools/apktool_executor.py " + dirPath + " " + unpackedPath
       call(UnpackCOMMAND, shell=True)
+
+      ManifestCOMMAND = "python " + appsrepoPath + "/tools/copy_manifest.py " + unpackedPath + " " + manifestPath
+      call(ManifestCOMMAND, shell=True)
+
+      CodeCOMMAND = "python " + appsrepoPath + "/smali-methods-finder/smali_invoked_methods.py " + unpackedPath + "/ " + codePath + "/"
+      call(CodeCOMMAND, shell=True)
+
+      uixmlCOMMAND = "python " + appsrepoPath + "/ui-xml/ui_xml.py " + unpackedPath + " -o " + uixmlPath
+      call(uixmlCOMMAND, shell=True)
+
+    break
+
