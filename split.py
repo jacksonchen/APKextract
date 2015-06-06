1c7a7806c5d15f3d7082f8ee2de0a195e50ad188
Jackson Chen <tiger2chen@gmail.com>
2015-06-06 15:21:55 -0600

    Correct folder organization

--- split.py
+++ split.py
@@ -2,22 +2,18 @@ import re
 import sys
 import os
 folder = ""
-counter = 0
-name = str(counter) + ".txt"
-fo = open(name, "w")
+name = ""
+fo = open(sys.argv[1], "r")
 
 with open(sys.argv[1]) as f:
   for line in f:
     m = re.search('/([^\/]+)/[^\/]+$', line)
     if m:
       if m.groups(1)[0] != folder:
-        print counter
         fo.close()
-        name = str(counter) + ".txt"
+        name = str(m.groups(1)[0]) + ".txt"
         fo = open(name, "w")
         fo.write(line)
         folder = m.groups(1)[0]
       else:
         fo.write(line)
-
-      counter += 1
